4819c84d5892c00c6bcedf050833dd8f2cc9c4e8
QuaRang <dbduddnd1225@gmail.com>
2024-09-04 16:27:05 +0900

    프로젝트 추가

---
+++ EX-VR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EX-VR
+//
+//  Created by 유영웅 on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EX-VR/EX_VRApp.swift
@@ -0,0 +1,17 @@
+//
+//  EX_VRApp.swift
+//  EX-VR
+//
+//  Created by 유영웅 on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EX_VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
